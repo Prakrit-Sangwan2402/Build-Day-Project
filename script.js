d78d94d38dbcbc12a9659cf7e633b73a3e731834
Prakrit Sangwan <sangwanprakrit@gmail.com>
2025-12-06 18:43:54 +0530

    Refactor task addition and deletion logic

--- script.js
+++ script.js
@@ -3,34 +3,30 @@ const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 const dingSound = document.getElementById("dingSound");
 
-addBtn.addEventListener("click", function addTask() {
+addBtn.addEventListener("click", () => {
   const taskText = taskInput.value.trim();
-  if (taskText === "") {
-    return;
-  }
+  if (taskText === "") return;
 
   const li = document.createElement("li");
   li.textContent = taskText;
 
   li.addEventListener("click", () => {
+    li.classList.toggle("done");
     if (li.classList.contains("done")) {
-      li.classList.remove("done");
-    } else {
-      li.classList.add("done");
       dingSound.play();
     }
-  })
+  });
 
   const del = document.createElement("span");
   del.textContent = "X";
   del.className = "delete";
-  del.addEventListener("click", (p) => {
-    p.stopPropagation();
+  del.addEventListener("click", (event) => {
+    event.stopPropagation();
     li.remove();
-  })
+  });
 
   li.appendChild(del);
   taskList.appendChild(li);
 
   taskInput.value = "";
-})
+});
